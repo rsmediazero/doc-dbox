b4af21cbb22e2a2ccaeea3a5ddce5dc7a61789b1
rsmediazero <107334806+rsmediazero@users.noreply.github.com>
2026-01-17 07:44:52 +0700

    [VOID-UPLOAD] 20260117_074451

---
+++ dramabox/dramaboxHelper.js
@@ -0,0 +1,89 @@
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+import crypto from "crypto";
+import { DramaboxApp } from "./sign.js";
+
+function randomAndroidId() {
+  return "ffffffff" + crypto.randomBytes(4).toString("hex") + "000000000";
+}
+
+export const getToken = async (payload, timestamp) => {
+  try {
+    const url = `https://sapi.dramaboxdb.com/drama-box/ap001/bootstrap?timestamp=${timestamp}`;
+    const deviceId = uuidv4(35);
+    const androidId = randomAndroidId();
+    const headers = {
+      "User-Agent": "okhttp/4.10.0",
+      "Accept-Encoding": "gzip",
+      "Content-Type": "application/json",
+      "tn": "",
+      "version": "430",
+      "vn": "4.3.0",
+      "cid": "DRA1000042",
+      "package-name": "com.storymatrix.drama",
+      "apn": "1",
+      "device-id": deviceId,
+      "android-id": androidId,
+      "language": "in",
+      "current-language": "in",
+      "p": "43",
+      "time-zone": "+0800",
+      "content-type": "application/json; charset=UTF-8"
+    };
+
+    const body = JSON.stringify({ distinctId: null });
+    const res = await axios.post(url, { distinctId: null }, {headers});
+
+    if(!res.data?.data?.user){
+      return await getToken(payload, timestamp);
+    }
+    const tokenData = {
+      token: res.data.data.user.token,
+      deviceId,
+      androidId,
+      uuid: res.data.data.user.uid,
+      attributionPubParam: res.data.data.attributionPubParam,
+      timestamp: Date.now(),
+    };
+
+    return tokenData;
+  } catch (error) {
+    console.error("[ERROR] Gagal mengambil token:", error);
+    throw error;
+  }
+};
+
+
+
+
+/**
+ * Generate headers lengkap siap pakai
+ */
+export const getHeaders = async (payload, timestamp) => {
+  const { token, deviceId, androidId, uuid} = await getToken(payload, timestamp);
+  const headers = {
+    "Host": "sapi.dramaboxdb.com",
+    "tn": `Bearer ${token}`,
+    "Version": "430",
+    "Vn": "4.3.0",
+    "Cid": "DALPF1057826",
+    "Package-Name": "com.storymatrix.drama",
+    "Apn": "1",
+    "device-id": deviceId,
+    "Language": "in",
+    "Current-Language": "in",
+    "P": "43",
+    "Time-Zone": "+0800",
+    "md": "Redmi Note 8",
+    "Ov": "14",
+    "android-id": androidId,
+    "Mf": "XIAOMI",
+    "Brand": "Xiaomi",
+    "Content-Type": "application/json; charset=UTF-8",
+    "User-Agent": "okhttp/4.10.0",
+  };
+  const body = JSON.stringify(payload);
+  return headers;
+};
+
+export default { getToken, getHeaders };
