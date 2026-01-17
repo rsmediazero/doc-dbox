7eaea952b25fcd6c648f739c2ed40b13da111aed
rsmediazero <107334806+rsmediazero@users.noreply.github.com>
2026-01-17 07:44:54 +0700

    [VOID-UPLOAD] 20260117_074453

---
+++ dramabox/index.js
@@ -0,0 +1,18 @@
+import {getChapters} from './chapter.js';
+import {getDramaDetail} from './detail.js';
+import {getDetailsv2, batchDownload} from './details.js';
+import {getDramaList, getCategories, getBookFromCategories, getRecommendedBooks} from './drama.js';
+import {searchDrama, searchDramaIndex} from './search.js';
+
+export {
+    getChapters,
+    getDramaDetail,
+    getDetailsv2,
+    batchDownload,
+    getDramaList,
+    getCategories,
+    getBookFromCategories,
+    getRecommendedBooks,
+    searchDrama,
+    searchDramaIndex
+}
